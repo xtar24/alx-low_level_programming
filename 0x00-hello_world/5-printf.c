60e716be353cbace96bf2d3983be91b4879c8767
xtar24 <akinfetolulope@gmail.com>
2022-12-09 22:05:37 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,34 +1,11 @@
 #include <stdio.h>
-
 /**
-
- *main -is the entry point for the program.
-
+ * main - Entry Point for the program,
  *
-
- *Return:zero if no error, Non-zero value  if errors.
-
+ * Return: 0 if no errors, return non zero if errors
  */
-
 int main(void)
-
 {
-
-printf("Size of a char: %ld byte(s)
-", sizeof(char));
-
-printf("Size of an int: %ld byte(s)
-", sizeof(int));
-
-printf("Size of a long int: %ld byte(s)
-", sizeof(long int));
-
-printf("Size of a long long int: %ld byte(s)
-", sizeof(long long int));
-
-printf("Size of a float: %ld byte(s)
-", sizeof(float));
-
-return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
