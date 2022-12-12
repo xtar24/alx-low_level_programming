99a36866062a7546cdc173fe81af513c127a998a
xtar24 <akinfetolulope@gmail.com>
2022-12-11 19:02:39 -0800

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c)
+				;
+				c++;
+		}
+	while
+		(d <= 'z') {
+			putchar(c)
+				;
+				d++;
+		}
+	return (0);
+}
