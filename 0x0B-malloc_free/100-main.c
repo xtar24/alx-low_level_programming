06fb63583884ed2b1320689ca7a0ee8dde0111b3
xtar24 <akinfetolulope@gmail.com>
2023-03-16 08:27:06 -0700

    update

---
+++ 0x0B-malloc_free/100-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char *av[])
+{
+	char *s;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	s = argstostr(ac, av);
+	if (s == NULL)
+	{
+		return (1);
+	}
+	printf("%s", s);
+	free(s);
+	return (0);
+}
